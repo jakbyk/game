8ea614f206b29764588a67e4cd7dba75572e70bd
jakbyk <jakbyk@wp.pl>
2025-06-20 14:03:41 +0200

    test fixes

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,6 @@
 class GameController < ApplicationController
   def home
     redirect_to login_path, warning: "Musisz być zalogowany, aby zagrać", status: :see_other unless logged_in?
-    @plays = current_user.plays
+    @plays = current_user&.plays
   end
 end
--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class GameControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get game_home_url
-    assert_response :success
+    assert_response :see_other
   end
 end
