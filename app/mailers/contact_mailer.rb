3d8e4d432eb78c581fa06412118b0bd0c680b48d
jakbyk <jakbyk@wp.pl>
2025-08-10 15:58:10 +0200

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
-  def response_email(contact_message)
-    @contact = contact_message
+  def response_email(response)
+    @contact = response.contact_message
+    @message = response.message
     mail(to: @contact.email_to_respond, subject: "Odpowiedź od twórców gry \"Gra o Polskę\"")
   end
 end
