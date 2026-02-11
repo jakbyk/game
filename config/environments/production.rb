49e3543eaa445615564222901fea1c034697f5ec
jakbyk <jakbyk@wp.pl>
2026-02-11 14:08:35 +0100

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,16 +122,5 @@ Rails.application.configure do
   config.action_mailer.logger = Logger.new(STDOUT)
   config.action_mailer.logger.level = Logger::DEBUG
 
-  config.cache_store = :redis_cache_store, {
-    url: ENV.fetch("REDIS_URL"),
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER },
-    pool: {
-      size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5)),
-      timeout: 5
-    },
-    connect_timeout: 5,
-    read_timeout: 1,
-    write_timeout: 1,
-    reconnect_attempts: 2
-  }
+  config.cache_store = :memory_store
 end
